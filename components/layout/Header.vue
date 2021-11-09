cd8687483bc1b1c0e66cfa83edba46c64c572cde
Jeremy <jeremy@codeconut.fr>
2021-11-09 14:56:41 +0100

    header mobile

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -48,21 +48,21 @@
     <!-- Mobile menu, show/hide based on mobile menu state. -->
     <div>
       <transition
-        enter-class="duration-150 ease-out"
-        enter-active-class="opacity-0"
+        enter-active-class="duration-150 ease-out"
+        enter-class="opacity-0"
         enter-to-class="opacity-100"
-        leave-class="duration-150 ease-in"
-        leave-active-class="opacity-100"
+        leave-active-class="duration-150 ease-in"
+        leave-class="opacity-100"
         leave-to-class="opacity-0"
       >
         <div v-show="showMobileMenu" class="z-20 fixed inset-0 bg-black bg-opacity-25" aria-hidden="true" />
       </transition>
       <transition
-        enter-class="duration-150 ease-out"
-        enter-active-class="opacity-0 scale-95"
+        enter-active-class="duration-150 ease-out"
+        enter-class="opacity-0 scale-95"
         enter-to-class="opacity-100 scale-100"
-        leave-class="duration-150 ease-in"
-        leave-active-class="opacity-100 scale-100"
+        leave-active-class="duration-150 ease-in"
+        leave-class="opacity-100 scale-100"
         leave-to-class="opacity-0 scale-95"
       >
         <div v-show="showMobileMenu" class="z-30 absolute top-0 inset-x-0 max-w-3xl mx-auto w-full p-2 transition transform origin-top">
@@ -92,46 +92,18 @@
                 <a
                   v-for="link in navigation"
                   :key="link.name"
-                  class="block rounded-md px-3 py-2 text-base text-gray-900 font-medium hover:bg-gray-100 hover:text-gray-800"
+                  class="block rounded-md px-3 py-2 text-base text-jva-900 font-medium hover:bg-gray-100 hover:text-gray-800"
                   :href="link.href"
                 >{{ link.name }}</a>
               </div>
             </div>
-            <div class="pt-4 pb-2">
-              <div class="flex items-center px-5">
-                <div class="flex-shrink-0">
-                  <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-                </div>
-                <div class="ml-3 min-w-0 flex-1">
-                  <div class="text-base font-medium text-gray-800 truncate">
-                    Chelsea Hagon
-                  </div>
-                  <div class="text-sm font-medium text-gray-500 truncate">
-                    chelseahagon@example.com
-                  </div>
-                </div>
-                <button type="button" class="ml-auto flex-shrink-0 bg-white p-1 text-gray-400 rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500">
-                  <span class="sr-only">View notifications</span>
-                  <!-- Heroicon name: outline/bell -->
-                  <svg
-                    class="h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-                  </svg>
-                </button>
-              </div>
-              <div class="mt-3 px-2 space-y-1">
-                <a href="#" class="block rounded-md px-3 py-2 text-base text-gray-900 font-medium hover:bg-gray-100 hover:text-gray-800">Your Profile</a>
-
-                <a href="#" class="block rounded-md px-3 py-2 text-base text-gray-900 font-medium hover:bg-gray-100 hover:text-gray-800">Settings</a>
-
-                <a href="#" class="block rounded-md px-3 py-2 text-base text-gray-900 font-medium hover:bg-gray-100 hover:text-gray-800">Sign out</a>
-              </div>
+            <div class="mt-3 p-2">
+              <a
+                v-for="link in secondNavigation"
+                :key="link.name"
+                :href="link.href"
+                class="block rounded-md px-3 py-2 text-base text-cool-gray-600 font-medium hover:bg-gray-100 hover:text-gray-800"
+              >{{ link.name }}</a>
             </div>
           </div>
         </div>
@@ -147,11 +119,20 @@ const navigation = [
   { name: 'Connexion', href: '#', current: false }
 ]
 
+const secondNavigation = [
+  { name: 'Associations', href: '#', current: false },
+  { name: 'Territoires', href: '#', current: false },
+  { name: 'Écoles et universités', href: '#', current: false },
+  { name: 'Actualités', href: '#', current: false },
+  { name: 'Centre d\'aide', href: '#', current: false }
+]
+
 export default {
   data () {
     return {
       showMobileMenu: false,
-      navigation
+      navigation,
+      secondNavigation
     }
   }
 }
