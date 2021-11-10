1fab9a9553ec2286b197b666fe0be97e44b63dff
NVaissaud <n.vaissaud@gmail.com>
2021-11-10 12:53:18 +0100

    Badge in others

--- pages/components/base/buttons.vue
+++ pages/components/base/buttons.vue
@@ -152,63 +152,6 @@
         </Button>
       </div>
     </div>
-    <div class="flex flex-col space-y-4">
-      <div class="text-lg leading-6 font-medium text-gray-900">
-        Badges
-      </div>
-      <div class="flex flex-wrap gap-2 items-end">
-        <Badge color="gray">
-          Default
-        </Badge>
-        <Badge color="gray-light">
-          Default light
-        </Badge>
-        <Badge color="orange">
-          En attente de validation
-        </Badge>
-        <Badge color="green">
-          Validée
-        </Badge>
-        <Badge color="red">
-          Signalée
-        </Badge>
-      </div>
-      <div class="text-lg leading-6 font-medium text-gray-900">
-        Par domaine d'action
-      </div>
-      <div class="flex flex-wrap gap-2 items-end">
-        <Badge color="covid">
-          Covid
-        </Badge>
-        <Badge color="education">
-          Éducation
-        </Badge>
-        <Badge color="sante">
-          Santé
-        </Badge>
-        <Badge color="nature">
-          Nature
-        </Badge>
-        <Badge color="solidarite">
-          Solidarité
-        </Badge>
-        <Badge color="sport">
-          Sport
-        </Badge>
-        <Badge color="prevention">
-          Prévention
-        </Badge>
-        <Badge color="memoire">
-          Mémoire
-        </Badge>
-        <Badge color="cooperation">
-          Coopération
-        </Badge>
-        <Badge color="culture">
-          Culture
-        </Badge>
-      </div>
-    </div>
   </div>
 </template>
 
