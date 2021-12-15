4c0d01b6e9650fd6e085bbb7ad93773195d92468
Jeremy <jeremy@codeconut.fr>
2021-12-15 18:18:35 +0100

    wip

--- pages/admin/missions/index.vue
+++ pages/admin/missions/index.vue
@@ -8,15 +8,48 @@
     <template #sidebar>
       <div class="flex flex-col gap-y-4 sticky top-8">
         <div>
-          @todo
+          Types de mission<br>
+          domaines d'action<br>
+          Proposé aux, il faudrait le laisser pour les référents (ça servira lorsque le lien avec le SNU sera effectif<br>
+          Places restantes<br>
         </div>
+        <!-- <SelectAdvanced
+          name="statut_juridique"
+          placeholder="Statut juridique"
+          :options="structure_legal_status"
+          :value="$route.query['filter[statut_juridique]']"
+          variant="transparent"
+          clearable
+          @input="changeFilter('filter[statut_juridique]', $event)"
+        />
+        <SelectAdvanced
+          name="department"
+          placeholder="Département"
+          :options="departments"
+          :value="$route.query['filter[department]']"
+          variant="transparent"
+          clearable
+          @input="changeFilter('filter[department]', $event)"
+        /> -->
       </div>
     </template>
     <div>
       <Heading as="h1" :level="1">
         {{ queryResult.total }} missions
       </Heading>
-
+      <div class="hidden lg:flex gap-x-4 mt-6 text-sm overflow-y-auto pb-3">
+        <button :class="['px-4 py-1 flex-shrink-0', !$route.query['filter[state]'] ? 'shadow bg-white rounded-full text-gray-900 font-semibold' : 'text-gray-500 font-medium']" @click="deleteFilter('filter[state]')">
+          Toutes
+        </button>
+        <button
+          v-for="option in mission_states"
+          :key="option.key"
+          :class="['px-4 py-1 flex-shrink-0', $route.query['filter[state]'] && $route.query['filter[state]'] == option.key ? 'shadow bg-white rounded-full text-gray-900 font-semibold' : 'text-gray-500 font-medium']"
+          @click="changeFilter('filter[state]', option.key)"
+        >
+          {{ option.label }}
+        </button>
+      </div>
       <div class="my-6">
         <div v-for="mission in queryResult.data" :key="mission.id">
           <div class="flex justify-between font-gray-800">
@@ -37,6 +70,7 @@
 
 <script>
 import QueryBuilder from '@/mixins/query-builder'
+import labels from '@/utils/labels.json'
 
 export default {
   mixins: [QueryBuilder],
@@ -52,7 +86,8 @@ export default {
   },
   data () {
     return {
-      endpoint: '/missions'
+      endpoint: '/missions',
+      mission_states: labels.mission_workflow_states
     }
   }
 
