37ec722cb6de1f38cfc6942c4158881dac7850d8
Ijahman <kevin.vaissaud@gmail.com>
2022-02-09 16:53:48 +0100

    profile avatar

--- components/base/Avatar.vue
+++ components/base/Avatar.vue
@@ -8,8 +8,8 @@
       {'w-16 h-16': size == 'md'},
 
       // Background color
-      {'bg-jva-blue-500 ': backgroundColor == 'blue'},
-      {'bg-white ': backgroundColor == 'white'},
+      {'bg-jva-blue-500 ': backgroundColor == 'blue' && !srcset},
+      {'bg-white ': backgroundColor == 'white' && !srcset},
     ]"
   >
     <img
