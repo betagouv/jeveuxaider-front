c3f3376369faa94f71a8db8b3c61487394795e73
Ijahman <kevin.vaissaud@gmail.com>
2022-02-11 15:57:43 +0100

    scroll behavior correction

--- app/router.scrollBehavior.js
+++ app/router.scrollBehavior.js
@@ -1,3 +1,22 @@
-export default function (to, from, savedPosition) {
+export default async function (to, from, savedPosition) {
+  if (savedPosition) {
+    return savedPosition
+  }
+
+  const findEl = (hash, x) => {
+    return document.querySelector(hash) ||
+      new Promise((resolve, reject) => {
+        if (x > 50) {
+          return resolve()
+        }
+        setTimeout(() => { resolve(findEl(hash, ++x || 1)) }, 100)
+      })
+  }
+
+  if (to.hash) {
+    const el = await findEl(to.hash)
+    return { x: 0, y: el.offsetTop, behavior: 'smooth' }
+  }
+
   return { x: 0, y: 0 }
 }
