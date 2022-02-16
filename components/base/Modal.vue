358e2880de50f017c52be9010f443d41e38a174f
Jeremy <jeremy@codeconut.fr>
2022-02-16 17:56:24 +0100

    drawer marketplace

--- components/base/Modal.vue
+++ components/base/Modal.vue
@@ -25,7 +25,7 @@
               <div class="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
                 <button type="button" class="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cool-gray-500" @click="$emit('close')">
                   <span class="sr-only">Close</span>
-                  <XIcon />
+                  <XIcon @click="$emit('close')" />
                 </button>
               </div>
               <div class="sm:flex sm:items-start">
