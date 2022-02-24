33f273fb376540c1a05af345e8461b9e170ed14e
NVaissaud <n.vaissaud@gmail.com>
2022-02-24 17:46:40 +0100

    Fix banner z index

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -295,9 +295,7 @@ export default {
           { name: 'Organisations', to: '/admin/organisations', isActive: this.isActiveLink('/admin/organisations/*') },
           { name: 'Missions', to: '/admin/missions', isActive: this.isActiveLink('/admin/missions/*') },
           { name: 'Participations', to: '/admin/participations', isActive: this.isActiveLink('/admin/participations/*') },
-          { name: 'Utilisateurs', to: '/admin/utilisateurs', isActive: this.isActiveLink('/admin/utilisateurs/*') },
-          { name: 'TEMP TEMOGNAGES FORM', to: '/temoignages/JeD6nn2w0ZiZyUYH2r138I5YUPfJmK4Y', isActive: this.isActiveLink('/admin/utilisateurs/*') }
-
+          { name: 'Utilisateurs', to: '/admin/utilisateurs', isActive: this.isActiveLink('/admin/utilisateurs/*') }
         ]
       } else if (this.$store.getters.currentRole?.key === 'responsable') {
         return [
