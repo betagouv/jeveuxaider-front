3bf181695d48b47ad1ec875e7d8ab6f50c79e098
NVaissaud <n.vaissaud@gmail.com>
2022-03-09 12:36:33 +0100

    Export refactoring handleExport download csv file

--- mixins/export.js
+++ mixins/export.js
@@ -11,12 +11,23 @@ export default {
         return
       }
       this.exportLoading = true
-      const { data } = await this.$axios.get(this.exportEndpoint, {
+      await this.$axios.get(this.exportEndpoint, {
         params: { ...this.$route.query }
+      }).then((response) => {
+        const fileURL = window.URL.createObjectURL(new Blob([response.data]))
+        const fileLink = document.createElement('a')
+
+        fileLink.href = fileURL
+        fileLink.setAttribute('download', 'file.csv')
+        document.body.appendChild(fileLink)
+
+        fileLink.click()
+      }).catch((err) => {
+        console.log('err export', err)
       })
-      if (data) {
-        this.$toast.success("L'export est en cours.\nVous recevrez une notification lorsqu'il sera prêt.")
-      }
+      // if (data) {
+      //   this.$toast.success("L'export est en cours.\nVous recevrez une notification lorsqu'il sera prêt.")
+      // }
       this.exportLoading = false
     }
   }
