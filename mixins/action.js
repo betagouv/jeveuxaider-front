bd869fdd27ceb6fc9282c846fbe1d33c8038947c
NVaissaud <n.vaissaud@gmail.com>
2022-03-18 14:58:35 +0100

    Edit wording actions

--- mixins/action.js
+++ mixins/action.js
@@ -24,7 +24,7 @@ export default {
             icon: '💬',
             iconVariant: 'warning',
             title: `<b>${this.$options.filters.formatNumber(action.value)} message(s)</b> non lu(s)`,
-            subtitle: 'Vous avez de nouveaux messages en attente',
+            subtitle: 'Des personnes attendent votre réponse !',
             link: '/messages'
           }
         case 'profile_incomplete':
@@ -32,15 +32,15 @@ export default {
             icon: '✍',
             iconVariant: 'warning',
             title: 'Votre profil est <b>incomplet</b>',
-            subtitle: 'Champs manquants: <span class="lowercase">' + action.value.map(option => this.$options.filters.label(option, 'profile_fields')).join(', ') + '</span>',
+            subtitle: 'Mettez à jour vos informations afin de permettre aux organisations de vous proposer directement des missions.',
             link: '/profile/edit'
           }
         case 'search_missions':
           return {
             icon: '🔍',
             iconVariant: 'info',
-            title: 'Trouver votre prochaine mission à réaliser',
-            subtitle: 'Vous avez du temps libre et souhaitez aider les organisations ?',
+            title: 'Trouvez votre prochaine mission',
+            subtitle: 'Vous avez du temps libre et souhaitez contribuer à la solidarité ?',
             link: '/missions-benevolat'
           }
         case 'organisations_waiting_validation':
@@ -48,7 +48,7 @@ export default {
             icon: '✊',
             iconVariant: 'warning',
             title: `<b>${this.$options.filters.formatNumber(action.value)} ${this.$options.filters.pluralize(action.value, 'organisation', 'organisations', false)}</b> en attente de validation`,
-            subtitle: 'Des organisations souhaitent publier des missions',
+            subtitle: 'Elles n\'attendent que vous pour pouvoir publier leurs missions !',
             link: '/admin/organisations?filter[state]=En attente de validation'
           }
         case 'mission_template_waiting_validation':
@@ -56,7 +56,7 @@ export default {
             icon: '✊',
             iconVariant: 'warning',
             title: `<b>${this.$options.filters.formatNumber(action.value)} ${this.$options.filters.pluralize(action.value, 'modèle de mission', 'modèles de mission', false)}</b> en attente de validation`,
-            subtitle: 'Des responsables de réseau souhaitent publier des modèles de mission',
+            subtitle: 'Des responsables de réseau souhaitent mettre en ligne des modèles de mission.',
             link: '/admin/contenus/modeles-mission?filter[state]=waiting'
           }
         case 'territoires_waiting_validation':
@@ -64,7 +64,7 @@ export default {
             icon: '✊',
             iconVariant: 'warning',
             title: `<b>${this.$options.filters.formatNumber(action.value)} ${this.$options.filters.pluralize(action.value, 'territoire', 'territoires', false)}</b> en attente de validation`,
-            subtitle: 'Des territoires souhaient créer leur page',
+            subtitle: 'Des collectivités territoriales souhaitent créer leur page',
             link: '/admin/contenus/territoires?filter[state]=waiting'
           }
         case 'organisation_incomplete':
@@ -72,7 +72,7 @@ export default {
             icon: '✍',
             iconVariant: 'warning',
             title: 'Votre organisation est <b>incomplète</b>',
-            subtitle: 'Champs manquants: <span class="lowercase">' + action.value.map(option => this.$options.filters.label(option, 'structure_fields')).join(', ') + '</span>',
+            subtitle: 'Complétez les informations manquantes pour gagner en visibilité !',
             link: `/admin/organisations/${this.$store.getters.currentRole.contextable_id}/edit`
           }
         case 'missions_waiting_validation':
@@ -80,7 +80,7 @@ export default {
             icon: '✊',
             iconVariant: 'warning',
             title: `<b>${this.$options.filters.formatNumber(action.value)} ${this.$options.filters.pluralize(action.value, 'mission', 'missions', false)}</b> en attente de validation`,
-            subtitle: 'Des missions souhaitent être publiées sur la plateforme',
+            subtitle: 'Modérez les missions proposées afin qu\'elles soient publiées.',
             link: '/admin/missions?filter[state]=En attente de validation'
           }
         case 'missions_outdated':
@@ -88,15 +88,15 @@ export default {
             icon: '⏰️',
             iconVariant: 'danger',
             title: `<b>${this.$options.filters.formatNumber(action.value)} ${this.$options.filters.pluralize(action.value, 'mission', 'missions', false)}</b> dont la date de fin est passée`,
-            subtitle: 'Pensez à mettre à jour les missions',
+            subtitle: 'N\'oubliez pas de mettre à jour les dates de vos missions.',
             link: '/admin/missions'
           }
         case 'mission_new':
           return {
             icon: '✊',
             iconVariant: 'warning',
-            title: 'Créez votre première mission sur la plateforme',
-            subtitle: 'Aidez-vous de nos modèles de missions',
+            title: 'Publiez votre première mission sur JeVeuxAider.gouv.fr',
+            subtitle: 'Notre équipe se tient à votre disposition pour vous accompagner dans vos premiers pas !',
             link: `/admin/organisations/${this.$store.getters.currentRole.contextable_id}/missions/add`
           }
         case 'participations_waiting_validation':
@@ -104,7 +104,7 @@ export default {
             icon: '✊',
             iconVariant: 'warning',
             title: `<b>${this.$options.filters.formatNumber(action.value)} ${this.$options.filters.pluralize(action.value, 'participation', 'participations', false)}</b> en attente de validation`,
-            subtitle: 'Pensez à mettre à jour les missions',
+            subtitle: 'Des bénévoles attendent votre réponse pour s\'engager.',
             link: '/admin/participations'
           }
         case 'participations_in_progress':
@@ -112,7 +112,7 @@ export default {
             icon: '✊',
             iconVariant: 'warning',
             title: `<b>${this.$options.filters.formatNumber(action.value)} ${this.$options.filters.pluralize(action.value, 'participation', 'participations', false)}</b> en cours de traitement`,
-            subtitle: 'Pensez à traiter ces candidatures',
+            subtitle: 'Des bénévoles attendent votre réponse pour s\'engager.',
             link: '/admin/participations'
           }
         case 'snu_waiting_validation':
@@ -120,7 +120,7 @@ export default {
             icon: 'snu',
             iconVariant: 'snu',
             title: `<b>${this.$options.filters.formatNumber(action.value)} ${this.$options.filters.pluralize(action.value, 'candidature', 'candidatures', false)} SNU</b> en attente de validation`,
-            subtitle: 'Des jeunes volontaires souhaitent vous aider',
+            subtitle: 'Des jeunes volontaires en SNU souhaitent vous aider',
             href: action.href
           }
         case 'snu_contract_to_be_filled':
@@ -128,7 +128,7 @@ export default {
             icon: 'snu',
             iconVariant: 'snu',
             title: `<b>${this.$options.filters.formatNumber(action.value)} ${this.$options.filters.pluralize(action.value, "contrat d'engagement", "contrats d'engagement", false)} SNU</b> à éditer`,
-            subtitle: 'Formalité à remplir pour valider les candidatures de vos volontaires',
+            subtitle: 'Complétez les contrats d\'engagement pour accueillir des jeunes en SNU sur vos missions.',
             href: action.href
           }
         case 'snu_contract_to_be_signed':
@@ -136,7 +136,7 @@ export default {
             icon: 'snu',
             iconVariant: 'snu',
             title: `<b>${this.$options.filters.formatNumber(action.value)} ${this.$options.filters.pluralize(action.value, "contrat d'engagement", "contrats d'engagement", false)} SNU</b> à signer`,
-            subtitle: 'Formalité à signer pour valider les candidatures de vos volontaires',
+            subtitle: 'N\'oubliez pas de signer les contrats d\'engagement pour accueillir des jeunes en SNU sur vos missions.',
             href: action.href
           }
       }
