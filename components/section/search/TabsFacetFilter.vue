cf87af54fc41e15c0c8f527a9f82b0e9875a4bfe
Ijahman <kevin.vaissaud@gmail.com>
2022-05-19 11:42:14 +0200

    theme tab facet filters

--- components/section/search/TabsFacetFilter.vue
+++ components/section/search/TabsFacetFilter.vue
@@ -1,23 +1,28 @@
 <template>
-  <div class="shadow-xl rounded-xl overflow-hidden">
-    <div class="flex">
-      <div v-for="tab,i in tabs" :key="i" class="">
-        <div
+  <div class="flex">
+    <div v-for="tab,i in tabs" :key="i" class="">
+      <div
+        :class="[
+          'flex justify-center items-center px-8 py-6 cursor-pointer space-x-2',
+          isActiveFilter(filterName, tab.filterValue) || tab.current ? 'bg-white shadow-xl' : 'shadow-inner-xl',
+          {'rounded-l-xl': i == 0},
+          {'rounded-r-xl': i == 1},
+        ]"
+        @click="addFilter(filterName, tab.filterValue)"
+      >
+        <component
+          :is="tab.icon"
+          v-if="tab.icon"
           :class="[
-            'flex justify-center items-center px-8 py-6 cursor-pointer space-x-2',
-            isActiveFilter(filterName, tab.filterValue) || tab.current ? 'bg-white' : 'shadow-inner-xl'
-          ]"
-          @click="addFilter(filterName, tab.filterValue)"
+            isActiveFilter(filterName, tab.filterValue) || tab.current ? 'text-jva-green-500' : 'text-gray-400 group-hover:text-gray-500', 'h-5 w-5']"
+          aria-hidden="true"
+        />
+        <span
+          class="font-bold"
+          :class="[isActiveFilter(filterName, tab.filterValue) || tab.current ? 'text-gray-700': 'text-gray-400']"
         >
-          <component
-            :is="tab.icon"
-            v-if="tab.icon"
-            :class="[
-              isActiveFilter(filterName, tab.filterValue) || tab.current ? 'text-jva-green-500' : 'text-gray-400 group-hover:text-gray-500', 'h-5 w-5']"
-            aria-hidden="true"
-          />
-          <span class="font-bold text-gray-700 ">{{ tab.label || tab.filterValue }}<span /></span>
-        </div>
+          {{ tab.label || tab.filterValue }}
+        </span>
       </div>
     </div>
   </div>
