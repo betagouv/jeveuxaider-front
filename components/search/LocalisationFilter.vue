b692a7e9774e56c4a875589c75c72d8845dc0cfd
NVaissaud <n.vaissaud@gmail.com>
2022-05-19 19:31:08 +0200

    LocalisationFilter

--- components/search/LocalisationFilter.vue
+++ components/search/LocalisationFilter.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
     <div class="group flex justify-between items-center cursor-pointer" @click="isOpen = !isOpen">
-      <slot name="button" :isOpen="isOpen">
-        Localisation slot
-      </slot>
+      <div class="text-gray-900">
+        <div v-if="ipLatLng">
+          Près de chez moi <span v-if="ipCity">({{ ipCity }})</span>
+        </div>
+        <div v-else>
+          {{ $route.query.city }}
+        </div>
+      </div>
       <ChevronDownIcon class="text-gray-500 h-4 w-4 group-hover:text-gray-900" />
     </div>
     <div v-if="isOpen" v-click-outside="onClickOutside">
@@ -20,13 +25,21 @@
             <XIcon v-if="searchValue" class="text-gray-500 h-5 w-5" @click="reset" />
           </div>
           <div class="py-4 flex flex-col space-y-1">
-            <div v-for="suggestion,i in suggestions" :key="i" class="px-4 py-1 text-gray-600 hover:bg-gray-50 cursor-pointer flex justify-between truncate flex-1">
+            <div v-for="suggestion,i in suggestions" :key="i" class="px-4 py-1 text-gray-600 hover:bg-gray-50 cursor-pointer flex justify-between truncate flex-1" @click="handleSelectedAdress(suggestion)">
               <div class="truncate">
-                {{ suggestion.name }}
+                {{ suggestion.city }}
               </div>
               <div class="text-gray-600 ml-1 font-light">
                 {{ suggestion.postcode }}
               </div>
+            </div>
+          </div>
+          <div class="border-t px-6 py-4 flex justify-between">
+            <div v-if="$route.query.city" class="text-gray-600 cursor-pointer" @click="handleSelectedAdress(null)">
+              Effacer
+            </div>
+            <div class="ml-auto text-jva-blue-500 cursor-pointer" @click="isOpen = false">
+              Valider
             </div>
           </div>
         </div>
@@ -37,8 +50,10 @@
 
 <script>
 import { debounce } from 'lodash'
+import AlgoliaQueryBuilder from '@/mixins/algolia-query-builder'
 
 export default {
+  mixins: [AlgoliaQueryBuilder],
   props: {
     label: {
       type: String,
@@ -52,34 +67,40 @@ export default {
   data () {
     return {
       isOpen: false,
-      searchValue: null,
+      ipCity: null,
+      searchValue: this.$route.query.city,
       fetchSuggestions: [],
       initialSuggestions: [
         {
-          name: 'Paris',
-          aroundLatLng: '48.859,2.347'
+          city: 'Paris',
+          coordinates: ['2.347', '48.859']
         },
         {
-          name: 'Lyon',
-          aroundLatLng: '45.758,4.835'
+          city: 'Lyon',
+          coordinates: ['4.835', '45.758']
         },
         {
-          name: 'Marseille',
-          aroundLatLng: '43.282,5.405'
+          city: 'Marseille',
+          coordinates: ['5.405', '43.282']
         }
       ]
     }
   },
-  //   async mounted () {
-  //     if (this.ipLatLng) {
-  //       const [lat, lng] = this.ipLatLng.split(',')
-  //       const res = await this.$axios.get(`https://api-adresse.data.gouv.fr/reverse/?lon=${lng}&lat=${lat}`)
-  //       this.ipCity = res.data?.features[0]?.properties?.city
-  //     }
-  //   },
   computed: {
     suggestions () {
       return this.fetchSuggestions.length ? this.fetchSuggestions : this.initialSuggestions
+    }
+  },
+  watch: {
+    '$route.query.city' (newVal) {
+      this.searchValue = newVal
+    }
+  },
+  async mounted () {
+    if (this.ipLatLng) {
+      const [lat, lng] = this.ipLatLng.split(',')
+      const res = await this.$axios.get(`https://api-adresse.data.gouv.fr/reverse/?lon=${lng}&lat=${lat}`)
+      this.ipCity = res.data?.features[0]?.properties?.city
     }
   },
   methods: {
@@ -117,6 +138,20 @@ export default {
         this.fetchGeoSuggestions()
       }, 275)
       this.timeout()
+    },
+    handleSelectedAdress (suggestion) {
+      if (suggestion === null) {
+        this.$router.push({
+          path: this.$route.path,
+          query: { ...this.$route.query, aroundLatLng: undefined, city: undefined, page: undefined }
+        })
+      } else {
+        this.$router.push({
+          path: this.$route.path,
+          query: { ...this.$route.query, aroundLatLng: `${suggestion.coordinates[1]},${suggestion.coordinates[0]}`, city: suggestion.city, page: undefined }
+        })
+      }
+      this.isOpen = false
     }
   }
 }
