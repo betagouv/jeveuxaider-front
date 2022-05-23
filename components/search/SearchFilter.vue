2813bb098daa80948d2ce3a0927456d0147d1747
Ijahman <kevin.vaissaud@gmail.com>
2022-05-23 16:23:37 +0200

    debounce mots clés

--- components/search/SearchFilter.vue
+++ components/search/SearchFilter.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="flex space-x-2 items-center">
     <SearchIcon class="text-gray-900 h-5 w-5" />
-    <input :value="searchValue" type="text" placeholder="Recherche..." class="pl-0 flex-1 border-0 w-full bg-transparent py-0 focus:ring-0" @input="handleInput">
-    <XIcon v-if="searchValue" class="text-gray-500 h-5 w-5 cursor-pointer hover:text-gray-900" @click="reset" />
+    <input
+      type="text"
+      placeholder="Recherche..."
+      class="pl-0 flex-1 border-0 w-full bg-transparent py-0 focus:ring-0"
+      :value="searchValue"
+      @input="handleInput"
+    >
+    <XIcon
+      v-if="searchValue"
+      class="text-gray-500 h-5 w-5 cursor-pointer hover:text-gray-900"
+      @click="reset"
+    />
   </div>
 </template>
 
 <script>
+import { debounce } from 'lodash'
 import AlgoliaQueryBuilder from '@/mixins/algolia-query-builder'
 
 export default {
@@ -23,7 +34,15 @@ export default {
   },
   methods: {
     handleInput (evt) {
-      this.addFilter('search', evt.target.value)
+      this.searchTerm = evt.target.value
+      this.showOptions = true
+      if (this.timeout) {
+        this.timeout.cancel()
+      }
+      this.timeout = debounce(() => {
+        this.addFilter('search', evt.target.value)
+      }, 275)
+      this.timeout()
     },
     reset () {
       this.searchValue = null
