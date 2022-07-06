1024b23d3200f1ec56544aceb09d0748f19f229b
Ijahman <kevin.vaissaud@gmail.com>
2022-07-06 12:30:02 +0200

    fix letter spacing, precision zone d'intervention

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,9 @@ export default {
     htmlAttrs: {
       lang: 'fr'
     },
+    bodyAttrs: {
+      class: 'text-base'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
