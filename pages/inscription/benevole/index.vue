83869049bc11434af6eb2b91704ac4ff600484c6
Jérémy Pinto <pintojeremy@gmail.com>
2022-08-22 16:28:48 +0200

    update stats

--- pages/inscription/benevole/index.vue
+++ pages/inscription/benevole/index.vue
@@ -81,7 +81,7 @@
             <p
               class="pt-10 pb-16 lg:pb-0 leading-10 text-4xl font-medium text-white sm:mt-5 tracking-tight"
             >
-              Plus de <b class="font-bold">300 000 bénévoles</b> <br>sont
+              Plus de <b class="font-bold">380 000 bénévoles</b> <br>sont
               déjà inscrits.
             </p>
           </div>
@@ -333,7 +333,7 @@
         <h3
           class="text-center leading-8 pb-8 text-gray-900 font-medium text-3xl tracking-tight px-4"
         >
-          Plus de <b class="font-bold">8000 organisations</b> ont déjà rejoint
+          Plus de <b class="font-bold">9000 organisations</b> ont déjà rejoint
           JeVeuxAider.gouv.fr
         </h3>
         <div class="overflow-hidden">
