016851d3353b085930db127e8adde8b3c4ac1bc5
Ijahman <kevin.vaissaud@gmail.com>
2022-08-25 13:29:37 +0200

    bandeau participations en attente

--- components/layout/HeaderBanner.vue
+++ components/layout/HeaderBanner.vue
@@ -18,14 +18,49 @@
         </nuxt-link>
       </template>
     </Banner>
+
+    <Banner v-if="isResponsable && nbWaitingParticipations > 0" icon="ExclamationIcon" class="!bg-red-600">
+      Vous avez {{ $options.filters.pluralize(nbWaitingParticipations, 'participation', 'participations') }} en cours de modération. Merci de mettre à jour le statut des candidatures reçues.
+      <template #action>
+        <nuxt-link :to="`/admin/participations?filter[state]=En attente de validation&filter[ofResponsable]=${$store.getters.profile.id}`">
+          <Button variant="white">
+            Modérer les participations
+          </Button>
+        </nuxt-link>
+      </template>
+    </Banner>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      userActions: null
+    }
+  },
+  async fetch () {
+    if (this.isResponsable) {
+      const { data: actions } = await this.$axios.get(
+        '/user/actions'
+      )
+      this.userActions = actions
+    }
+  },
   computed: {
     isImpersonate () {
       return this.$cookies.get('access-token-impersonate')
+    },
+    isResponsable () {
+      return this.$store.getters.currentRole?.key === 'responsable'
+    },
+    nbWaitingParticipations () {
+      return this.userActions?.find(action => action.type === 'participations_waiting_validation')?.value ?? 0
+    }
+  },
+  watch: {
+    '$store.state.auth.user.id' () {
+      this.$fetch()
     }
   }
 }
