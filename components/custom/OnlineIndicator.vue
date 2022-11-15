02ce4e0f5e956c8588fbaad212f3704335bb3d8e
Ijahman <kevin.vaissaud@gmail.com>
2022-11-15 11:50:47 +0100

    dsfr online indicator

--- components/custom/OnlineIndicator.vue
+++ components/custom/OnlineIndicator.vue
@@ -1,27 +1,19 @@
 <template>
-  <div>
-    <nuxt-link v-if="link" :to="link" class="flex items-center space-x-2" target="_blank">
-      <div
-        :class="['h-3 w-3 rounded-full', published ? 'bg-jva-green-500' : 'bg-jva-red-500']"
-      />
-      <div class="underline text-sm">
-        {{ published ? publishedLabel : unpublishedLabel }}
-      </div>
-      <ExternalLinkIcon class="h-4 w-4" />
-    </nuxt-link>
-    <div v-else class="flex items-center space-x-2">
-      <div
-        :class="['h-3 w-3 rounded-full', published ? 'bg-jva-green-500' : 'bg-jva-red-500']"
-      />
-      <div class="text-sm">
-        {{ published ? publishedLabel : unpublishedLabel }}
-      </div>
-    </div>
+  <div class="inline-flex items-center space-x-2">
+    <div :class="['h-3 w-3 rounded-full', published ? 'bg-jva-green-500' : 'bg-jva-red-500']" />
+    <component :is="link ? 'Link' : 'span'" v-if="link" :to="link" :is-external="true" class="text-sm">
+      {{ published ? publishedLabel : unpublishedLabel }}
+    </component>
   </div>
 </template>
 
 <script>
+import Link from '@/components/dsfr/Link.vue'
+
 export default {
+  components: {
+    Link
+  },
   props: {
     published: {
       type: Boolean,
