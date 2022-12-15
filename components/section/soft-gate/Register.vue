f088ecb8d94f1eb313f100d4ad047486506afcaf
Jérémy Pinto <pintojeremy@gmail.com>
2022-12-15 14:20:20 +0100

    fix

--- components/section/soft-gate/Register.vue
+++ components/section/soft-gate/Register.vue
@@ -180,12 +180,9 @@
           <Link
             to="/charte-reserve-civique"
             :is-external="true"
-            <<<<<<<
-            h-e-a-d="======"
             :icon-size="12"
             icon-class="mb-[.5px]"
           >
-            >>>>>> prod
             >
             charte
           </Link>
