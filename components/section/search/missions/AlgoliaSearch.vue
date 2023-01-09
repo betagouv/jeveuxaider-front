e112b6b2a276876e72c0dd8af201edea14f82f40
Ijahman <kevin.vaissaud@gmail.com>
2023-01-09 17:06:42 +0100

    loading geoloc par navigateur seulement si pas de query aroundLatLng

--- components/section/search/missions/AlgoliaSearch.vue
+++ components/section/search/missions/AlgoliaSearch.vue
@@ -173,7 +173,9 @@ export default {
   },
   mounted () {
     if (navigator.geolocation && !this.$store.state.algoliaSearch.aroundLatLng && !this.$store.state.algoliaSearch.navigatorGeolocation) {
-      this.$store.commit('algoliaSearch/setLoadingNavigatorGeolocation', true)
+      if (!this.$route.query.aroundLatLng) {
+        this.$store.commit('algoliaSearch/setLoadingNavigatorGeolocation', true)
+      }
       navigator.geolocation.getCurrentPosition(this.onNavigatorGeolocation, this.onNavigatorGeolocationError)
     }
   },
