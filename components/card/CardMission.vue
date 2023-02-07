33a521158d095648facb3f57b360560baf9a598b
Jérémy Pinto <pintojeremy@gmail.com>
2023-02-07 17:27:32 +0100

    show registration close on card mission

--- components/card/CardMission.vue
+++ components/card/CardMission.vue
@@ -177,7 +177,9 @@ export default {
       return null
     },
     placesLeftText () {
-      if (
+      if (!this.mission.is_registration_open) {
+        return 'Inscriptions fermées'
+      } else if (
         this.mission.publisher_name &&
         this.mission.publisher_name !== 'Réserve Civique' &&
         this.mission.places_left > 99
