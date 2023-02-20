b565ea20fbf6b5f61163bf8093b910a6263ede53
NVaissaud <n.vaissaud@gmail.com>
2023-02-20 13:12:59 +0100

    UtilisateursByAge 16 years

--- components/statistics/UtilisateursByAge.vue
+++ components/statistics/UtilisateursByAge.vue
@@ -1,8 +1,8 @@
 <template>
   <Box padding="sm" :loading="loading" loading-text="Générations des données...">
-    <BoxHeadingStatistics title="Âge des bénévoles par rapport à leur date d'inscription" class="mb-6" infos-bulle="Âge des bénévoles selon leur date d'inscription sur JeVeuxAider.gouv.fr" />
+    <BoxHeadingStatistics title="Répartition des bénévoles en fonction de leur âge" class="mb-6" infos-bulle="Répartition par âge des bénévoles inscrits sur la période sélectionnée" />
     <div v-if="statistics" class="flex flex-col gap-2">
-      <ListItemCount color="bg-yellow-900" label="Entre 15 et 19 ans" :count="statistics.BETWEEN_15_AND_20" display="count_percent" :total="total" />
+      <ListItemCount color="bg-yellow-900" label="Entre 16 et 19 ans" :count="statistics.BETWEEN_16_AND_20" display="count_percent" :total="total" />
       <ListItemCount color="bg-yellow-800" label="Entre 20 et 24 ans" :count="statistics.BETWEEN_20_AND_25" display="count_percent" :total="total" />
       <ListItemCount color="bg-yellow-700" label="Entre 25 et 29 ans" :count="statistics.BETWEEN_25_AND_30" display="count_percent" :total="total" />
       <ListItemCount color="bg-yellow-600" label="Entre 30 et 34 ans" :count="statistics.BETWEEN_30_AND_35" display="count_percent" :total="total" />
