7f804731e6750e4dbbdc63d81768c4abcae28245
Ijahman <kevin.vaissaud@gmail.com>
2023-03-10 17:06:31 +0100

    Bloc inscription benevole

--- components/section/home/BlocInscriptionBenevole.vue
+++ components/section/home/BlocInscriptionBenevole.vue
@@ -1,33 +1,33 @@
 <template>
-  <div class="relative p-4">
-    <div
-      class="relative overflow-hidden safari-fix-scale h-full flex flex-col"
-    >
+  <div class="container py-4">
+    <div class="relative overflow-hidden h-full flex flex-col">
       <img
-        srcset="/images/home/bg-inscription-benevole.jpg, /images/home/bg-inscription-benevole@2x.jpg 2x, /images/home/bg-inscription-benevole.webp, /images/home/bg-inscription-benevole@2x.webp 2x"
-        alt="Créer mon espace Bénévole"
-        class="w-full object-cover object-center absolute min-h-[568px] h-full"
+        srcset="
+          /images/home/bg-inscription-benevole.webp, /images/home/bg-inscription-benevole@2x.webp 2x,
+          /images/home/bg-inscription-benevole.jpg, /images/home/bg-inscription-benevole@2x.jpg 2x"
+        alt=""
         width="343"
         height="568"
+        class="w-full pointer-events-none object-cover object-top absolute min-h-[568px] h-full"
       >
-      <div class="relative text-white px-8 pt-16 pb-24">
-        <div class="text-5xl font-bold mb-6 leading-[56px]">
-          <div>Liberté<span class="text-jva-orange-300">.</span></div>
-          <div>Égalité<span class="text-jva-orange-300">.</span></div>
-          <div>Je veux aider<span class="text-jva-orange-300">.</span></div>
-        </div>
+      <div class="relative text-white px-8 pt-12 pb-16">
+        <Heading as="h2" size="alt-lg" class="mb-6" color="text-white">
+          <span class="block">Liberté<span class="text-jva-orange-300">.</span></span>
+          <span class="block">Égalité<span class="text-jva-orange-300">.</span></span>
+          <span class="block">Je veux aider<span class="text-jva-orange-300">.</span></span>
+        </Heading>
         <div class="mb-8">
           <div class="text-5xl font-bold leading-[56px]">
             100 000
           </div>
-          <div class="text-2xl font-bold">
+          <div class="text-xl xxs:text-2xl font-bold">
             bénévoles recherchés
           </div>
         </div>
-        <Button type="tertiary-no-outline" icon="RiUserHeartLine" @click="handleclick()">
-          Créer mon espace Bénévole
+        <Button type="tertiary-no-outline" icon="RiUserHeartLine" @click.native="onClick()">
+          Créer mon espace <span class="hidden xxs:inline"> Bénévole</span>
         </Button>
-        <div class="text-lg mt-2">
+        <div class="text-lg mt-3">
           Inscription en 2 minutes
         </div>
       </div>
@@ -37,13 +37,15 @@
 
 <script>
 import Button from '@/components/dsfr/Button.vue'
+import Heading from '@/components/dsfr/Heading.vue'
 
 export default {
   components: {
-    Button
+    Button,
+    Heading
   },
   methods: {
-    handleClick () {
+    onClick () {
       window.plausible &&
         window.plausible('Click CTA - Homepage - Créer mon espace Bénévole', {
           props: { isLogged: this.$store.getters.isLogged }
@@ -53,7 +55,3 @@ export default {
   }
 }
 </script>
-
-<style>
-
-</style>
