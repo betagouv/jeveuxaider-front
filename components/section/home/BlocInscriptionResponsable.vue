8aef13b202a8b4fd2d56dca9d890ab21cf13ae79
Ijahman <kevin.vaissaud@gmail.com>
2023-03-10 17:56:38 +0100

    Bloc inscription responsable

--- components/section/home/BlocInscriptionResponsable.vue
+++ components/section/home/BlocInscriptionResponsable.vue
@@ -1,41 +1,52 @@
 <template>
-  <div class="relative p-4">
-    <div class="relative h-[525px] bg-white px-8 pt-16 pb-24">
-      <div class="text-5xl font-bold mb-6 leading-[56px] text-center">
-        <span class="relative"><img class="absolute left-[-26px] top-[-5px]" src="/images/home/sparkle-left.svg"> Ils sont là.</span>
-      </div>
-      <div class="text-xl leading-7 mb-6 text-center">
-        Tous les acteurs du bénévolat vous attendent sur la première <strong>plateforme nationale de l’engagement</strong>.
-      </div>
-      <div class="text-center">
-        <Button @click="handleClick()">
-          Inscrivez votre organisation
-        </Button>
+  <div class="container">
+    <div class="relative bg-white">
+      <div class="px-4 pt-8">
+        <Heading as="h2" size="alt-md" class="mb-6 text-center">
+          <span class="relative">
+            <img
+              aria-hidden="true"
+              src="/images/home/sparkle-left.svg"
+              width="37"
+              height="33"
+              class="absolute left-[-26px] top-[-10px] xxs:-top-2 pointer-events-none"
+            >
+            <span>Ils sont là.</span>
+          </span>
+        </Heading>
+        <p class="text-xl leading-7 mb-6 text-center">
+          Tous les acteurs du bénévolat vous attendent sur la première <strong>plateforme nationale de l’engagement</strong>.
+        </p>
+        <div class="text-center mb-8">
+          <Button @click="handleClick()">
+            Inscrivez votre organisation
+          </Button>
+        </div>
       </div>
 
-      <div class="absolute bottom-0 right-0 left-0">
-        <img
-          srcset="
-          /images/home/trombi-organisations.webp,
-          /images/home/trombi-organisations@2x.webp 2x,
-          /images/home/trombi-organisations.png,
-          /images/home/trombi-organisations@2x.png 2x
+      <img
+        srcset="
+          /images/home/trombi-organisations.webp, /images/home/trombi-organisations@2x.webp 2x,
+          /images/home/trombi-organisations.png, /images/home/trombi-organisations@2x.png 2x
         "
-          alt="Trombinoscope organisations"
-          class=""
-          data-not-lazy
-        >
-      </div>
+        src="/images/home/trombi-organisations.png"
+        alt=""
+        width="425"
+        height="188"
+        class="object-cover object-top w-full"
+      >
     </div>
   </div>
 </template>
 
 <script>
 import Button from '@/components/dsfr/Button.vue'
+import Heading from '@/components/dsfr/Heading.vue'
 
 export default {
   components: {
-    Button
+    Button,
+    Heading
   },
   handleClick () {
     window.plausible &&
@@ -46,7 +57,3 @@ export default {
   }
 }
 </script>
-
-<style>
-
-</style>
