5b92d249bb995fe648b91022a49d688e0c4ecc06
Ijahman <kevin.vaissaud@gmail.com>
2023-03-13 12:51:40 +0100

    bloc activities

--- components/section/home/BlocActivities.vue
+++ components/section/home/BlocActivities.vue
@@ -14,37 +14,39 @@
         <RiArrowDownLine class="text-[#F9F6F2] w-8 fill-current flex-none" />
       </div>
     </div>
-    <div class="bg-jva-orange-300 px-4 py-12">
-      <Heading as="h3" size="alt-sm" class="mb-6 text-[#522F29]">
-        Le bénévolat et plus si affinités
-      </Heading>
-      <p class="text-[#522F29] text-xl leading-tight">
-        Contribuez à une cause qui vous touche au coeur
-      </p>
-      <div class="overflow-hidden mt-12">
-        <Slideshow
-          :slides-are-links="true"
-          :slides-count="activities.length"
-          :aria-labelledby="`label-activities-action-${_uid}`"
-          dots-variant="light"
-          :variable-width="true"
-        >
-          <!-- @todo: w-full ne marhce pas !!!! -->
-          <div v-for="(group,i) in activitiesGroups" :key="i" class="slide-wrapper flex flex-col space-y-4">
-            <div
-              v-for="activity in group"
-              :key="activity.key"
-            >
-              <nuxt-link
-                :to="`/missions-benevolat?activity.name=${activity.name}`"
-                class="inline-flex p-4 bg-white shadow text-lg font-bold w-full"
+    <div class="bg-jva-orange-300 py-12 overflow-hidden">
+      <div class="container">
+        <Heading as="h3" size="alt-sm" class="mb-6 text-[#522F29]">
+          Le bénévolat et plus si affinités
+        </Heading>
+        <p class="text-[#522F29] text-xl leading-tight">
+          Contribuez à une cause qui vous touche au coeur
+        </p>
+        <div class="mt-12">
+          <!-- @todo: accessibilité -->
+          <Slideshow
+            :slides-count="activities.length"
+            :aria-labelledby="`label-activities-action-${_uid}`"
+            dots-variant="light"
+            :adaptive-height="true"
+            class="sm:overflow-hidden"
+          >
+            <div v-for="(group,i) in activitiesGroups" :key="i" class="!flex flex-col gap-4 sm:flex-row flex-wrap">
+              <div
+                v-for="activity in group"
+                :key="activity.key"
               >
-                <span aria-hidden="true" class="flex-none">{{ activity.icon }}</span>
-                <span class="ml-3">{{ activity.name }}</span>
-              </nuxt-link>
+                <nuxt-link
+                  :to="`/missions-benevolat?activity.name=${activity.name}`"
+                  class="inline-flex p-4 bg-white shadow text-lg font-bold w-full sm:w-auto"
+                >
+                  <span aria-hidden="true" class="flex-none">{{ activity.icon }}</span>
+                  <span class="ml-3">{{ activity.name }}</span>
+                </nuxt-link>
+              </div>
             </div>
-          </div>
-        </Slideshow>
+          </Slideshow>
+        </div>
       </div>
     </div>
   </div>
@@ -76,7 +78,12 @@ export default {
 </script>
 
 <style lang="postcss" scoped>
-.slide-wrapper {
-  width: calc(100vw - 2rem) !important;
+:deep(.slick-track) {
+  width: 100% !important;
+}
+
+:deep(.slick-slide) {
+  width: 100%;
+  flex: none;
 }
 </style>
