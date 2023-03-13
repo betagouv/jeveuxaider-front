d8ab96ab0290cb649b7e61bdcc149d22c102b10c
Ijahman <kevin.vaissaud@gmail.com>
2023-03-13 18:00:35 +0100

    bloc actualités

--- components/section/home/BlocActualites.vue
+++ components/section/home/BlocActualites.vue
@@ -1,31 +1,33 @@
 <template>
-  <div class="bg-white px-4 py-12">
-    <div id="label-slideshow-articles" class="text-[40px] font-bold mb-6 leading-10">
-      Les actualités de l’engagement
-    </div>
-    <div class="text-[#4D4D4D] text-xl">
-      Restez informé de l'actualité de tous ceux qui font vivre l'engagement en France
-    </div>
-    <div v-if="articles.length" class="overflow-hidden mt-12">
-      <Slideshow
-        :slides-are-links="true"
-        aria-labelledby="label-slideshow-articles"
-      >
-        <a
-          v-for="article in articles"
-          :key="article.id"
-          class="slide-wrapper"
-          :href="`${$config.blog.url}/${article.slug}`"
-          target="_blank"
+  <div class="bg-white py-12 overflow-hidden">
+    <div class="container">
+      <Heading id="label-slideshow-articles" as="h2" size="alt-xs" class="mb-6">
+        Les actualités de l’engagement
+      </Heading>
+      <p class="text-[#4D4D4D] text-xl">
+        Restez informé de l'actualité de tous ceux qui font vivre l'engagement en France
+      </p>
+      <div v-if="articles.length" class="mt-12">
+        <Slideshow
+          :slides-are-links="true"
+          aria-labelledby="label-slideshow-articles"
         >
-          <CardArticle :article="article" class="!h-full" />
-        </a>
-      </Slideshow>
-    </div>
-    <div class="mt-8 text-center">
-      <Button type="tertiary" @click="handleClick()">
-        Plus d’articles
-      </Button>
+          <a
+            v-for="article in articles"
+            :key="article.id"
+            class="slide-wrapper"
+            :href="`${$config.blog.url}/${article.slug}`"
+            target="_blank"
+          >
+            <CardArticle :article="article" class="!h-full" />
+          </a>
+        </Slideshow>
+      </div>
+      <div class="mt-8 text-center">
+        <Button type="tertiary" @click="handleClick()">
+          Plus d’articles
+        </Button>
+      </div>
     </div>
   </div>
 </template>
@@ -34,11 +36,13 @@
 import axios from 'axios'
 import Button from '@/components/dsfr/Button.vue'
 import CardArticle from '@/components/card/CardArticle.vue'
+import Heading from '@/components/dsfr/Heading.vue'
 
 export default {
   components: {
     Button,
-    CardArticle
+    CardArticle,
+    Heading
   },
   data () {
     return {
