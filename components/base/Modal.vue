101dda64c02fa8089881a8b190f125912ec42282
Ijahman <kevin.vaissaud@gmail.com>
2023-05-10 17:26:03 +0200

    correction transition modal, props click outside alert dialog

--- components/base/Modal.vue
+++ components/base/Modal.vue
@@ -8,7 +8,7 @@
     leave-class="opacity-100"
     leave-to-class="opacity-0"
   >
-    <div v-show="isOpen" class="fixed z-50 inset-0 overflow-y-auto overscroll-contain" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+    <div v-if="isOpen" class="fixed z-50 inset-0 overflow-y-auto overscroll-contain" aria-labelledby="modal-title" role="dialog" aria-modal="true">
       <FocusLoop :is-visible="isOpen" @keydown.native.esc="$emit('close')">
         <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
           <div
@@ -27,7 +27,6 @@
             leave-to-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
             <div
-              v-if="isOpen"
               v-click-outside="handleClickOutside"
               :class="[
                 {'overflow-hidden' : overflowHidden},
