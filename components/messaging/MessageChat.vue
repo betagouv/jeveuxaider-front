48d1a2154df3265c239177a1268f9172f4853914
NVaissaud <n.vaissaud@gmail.com>
2023-06-27 17:57:09 +0200

    Lien dans les messages

--- components/messaging/MessageChat.vue
+++ components/messaging/MessageChat.vue
@@ -26,11 +26,7 @@
             {'border-[#EDEDED] bg-[#F6F6F6] !rounded-bl-none': !isFromMe},
           ]"
         >
-          <nl2br
-            tag="p"
-            :text="message.content"
-            class-name="text-black break-word"
-          />
+          <div class="text-black whitespace-pre-line" v-html="messageFormatted" />
         </div>
         <div
           :class="[
@@ -54,11 +50,31 @@ export default {
     }
   },
   computed: {
+    messageFormatted () {
+      return this.linkify(this.message.content)
+    },
     user () {
       return this.message.from
     },
     isFromMe () {
       return this.message.from_id === this.$store.state.auth.user.id
+    }
+  },
+  methods: {
+    linkify (inputText) {
+      // URLs starting with http://, https://, or ftp://
+      const replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/gim
+      let replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank" class="break-all hover:underline">$1</a>')
+
+      // URLs starting with www. (without // before it, or it'd re-link the ones done above)
+      const replacePattern2 = /(^|[^/])(www\.[\S]+(\b|$))/gim
+      replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank" class="break-all hover:underline">$2</a>')
+
+      // Change email addresses to mailto:: links
+      // const replacePattern3 = /(([a-zA-Z0-9_\-.]+)@[a-zA-Z_]+?(?:\.[a-zA-Z]{2,6}))+/gim
+      // replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1" class="break-all hover:underline">$1</a>')
+
+      return replacedText
     }
   }
 }
