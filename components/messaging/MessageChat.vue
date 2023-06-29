9c83883932842f4faafd2278acdf1e922af862a4
NVaissaud <n.vaissaud@gmail.com>
2023-06-29 12:49:44 +0200

    MessageChat unerline link

--- components/messaging/MessageChat.vue
+++ components/messaging/MessageChat.vue
@@ -64,15 +64,15 @@ export default {
     linkify (inputText) {
       // URLs starting with http://, https://, or ftp://
       const replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/gim
-      let replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank" class="break-all hover:underline">$1</a>')
+      let replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank" class="break-all underline hover:text-gray-700">$1</a>')
 
       // URLs starting with www. (without // before it, or it'd re-link the ones done above)
       const replacePattern2 = /(^|[^/])(www\.[\S]+(\b|$))/gim
-      replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank" class="break-all hover:underline">$2</a>')
+      replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank" class="break-all underline hover:text-gray-700">$2</a>')
 
       // Change email addresses to mailto:: links
       // const replacePattern3 = /(([a-zA-Z0-9_\-.]+)@[a-zA-Z_]+?(?:\.[a-zA-Z]{2,6}))+/gim
-      // replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1" class="break-all hover:underline">$1</a>')
+      // replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1" class="break-all underline hover:text-gray-700">$1</a>')
 
       return replacedText
     }
