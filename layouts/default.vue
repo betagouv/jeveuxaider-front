684cf77253cd5cc7c83c17523cf2cc011956fc1e
Ijahman <kevin.vaissaud@gmail.com>
2023-09-22 18:38:16 +0200

    revert home

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="h-full">
-    <!-- <Header /> -->
+    <Header />
     <div id="teleport-mobile-filters" class="relative z-50"></div>
     <main id="contenuprincipal" role="main">
       <NuxtPage />
     </main>
     <div id="teleport-drawer" class="relative z-20"></div>
-    <!-- <Footer /> -->
+    <Footer />
     <div id="teleport-body-end"></div>
   </div>
 </template>
 
 <script>
-// import Header from '@/components/layout/Header.vue'
-// import Footer from '@/components/layout/Footer.vue'
-// import MixinPlausible from '@/mixins/plausible.js'
+import Header from '@/components/layout/Header.vue'
+import Footer from '@/components/layout/Footer.vue'
+import MixinPlausible from '@/mixins/plausible.js'
 
 export default {
   components: {
-    // Header,
-    // Footer,
+    Header,
+    Footer,
   },
-  // mixins: [MixinPlausible],
+  mixins: [MixinPlausible],
 }
 </script>
