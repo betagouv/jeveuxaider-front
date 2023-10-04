16980a98e2cad217bd5bd2b10224389d82343ffc
NVaissaud <n.vaissaud@gmail.com>
2023-10-04 13:03:30 +0200

    Header messagerie mobile

--- components/layout/HeaderMessagerie.vue
+++ components/layout/HeaderMessagerie.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="flex bg-white border-b justify-between items-center px-6 py-4">
-    <div class="flex items-center gap-8">
-      <div class="border-r pr-8 py-2">
+  <div class="flex bg-white border-b justify-between items-center px-4 py-2 lg:px-6 lg:py-4">
+    <div class="flex items-center gap-4 lg:gap-8">
+      <div class="border-r pr-4 lg:pr-8 py-2">
         <nuxt-link
           no-prefetch
           :to="$stores.auth.contextRole === 'volontaire' ? '/profile' : '/dashboard'"
-          class="flex gap-10 items-center group"
+          class="flex gap-4 lg:gap-10 items-center group"
         >
           <RiArrowLeftLine
-            class="h-8 w-8 text-[#929292] fill-current group-hover:text-jva-blue-500 transition-colors"
+            class="flex-none h-6 w-6 lg:h-8 lg:w-8 text-[#929292] fill-current group-hover:text-jva-blue-500 transition-colors"
           />
           <img src="@/assets/images/jeveuxaider-logo-short.svg" alt="" width="59" height="44" />
         </nuxt-link>
       </div>
       <div class="">
-        <h1 class="text-[22px] font-bold">Messagerie</h1>
-        <p class="text-[#666666]">
+        <h1 class="text-lg lg:text-[22px] font-bold">Messagerie</h1>
+        <p class="text-[#666666] text-sm lg:text-base">
           <template v-if="$stores.messaging.unreadMessagesCount === 0">
             Aucun nouveau message
           </template>
