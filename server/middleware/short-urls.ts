c0071562cbf5537294a1ecede20869227195891c
Ijahman <kevin.vaissaud@gmail.com>
2023-10-19 11:18:51 +0200

    redirection messages : ajout utm

--- server/middleware/short-urls.ts
+++ server/middleware/short-urls.ts
@@ -1,11 +1,13 @@
 export default defineEventHandler(async (event) => {
+  const runtimeConfig = useRuntimeConfig()
   let url
 
   if (event.node.req.url?.startsWith('/m/')) {
-    url = event.node.req.url.replace('/m/', '/messages/')
+    url = new URL(event.node.req.url.replace('/m/', '/messages/'), runtimeConfig.public.appUrl)
+    url.searchParams.set('utm_source', 'sms_transac')
   }
 
   if (url) {
-    await sendRedirect(event, url)
+    await sendRedirect(event, url.href)
   }
 })
