6ac34f277dbece43849b87af4f9dba3ad525a92b
Jeremy Pinto <pintojeremy@gmail.com>
2024-02-06 12:25:26 +0100

    Remove badge offline orga

--- components/section/organisation/Badges.vue
+++ components/section/organisation/Badges.vue
@@ -3,14 +3,6 @@
     <DsfrBadge size="sm" :type="badgeTypeOrganisationSate">
       {{ organisation.state }}
     </DsfrBadge>
-    <DsfrBadge size="sm" type="gray">
-      <div class="flex items-center">
-        <div
-          :class="['h-2 w-2  rounded-full mr-1', hasPageOnline ? 'bg-green-600' : 'bg-red-600']"
-        ></div>
-        {{ hasPageOnline ? 'En ligne' : 'Hors ligne' }}
-      </div>
-    </DsfrBadge>
   </div>
 </template>
 
