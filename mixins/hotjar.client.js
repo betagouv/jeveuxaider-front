14012daf973dd64f2a880174eb3eabd061a27a58
Ijahman <kevin.vaissaud@gmail.com>
2024-03-01 17:03:08 +0100

    hotjar only if accepted in axeptio

--- mixins/hotjar.client.js
+++ mixins/hotjar.client.js
@@ -1,32 +1,24 @@
 /* eslint-disable */
 export default {
   mounted() {
-    if (!this.includeHotjar) {
+    if (!this.shouldLaunchHotjar) {
       return
     }
 
-    // Hotjar script start
-    ;(function (h, o, t, j, a, r) {
-      h.hj =
-        h.hj ||
-        function () {
-          ;(h.hj.q = h.hj.q || []).push(arguments)
+    void 0 === window._axcb && (window._axcb = [])
+    window._axcb.push((axeptio) => {
+      axeptio.on('cookies:complete', (choices) => {
+        if (choices.hotjar) {
+          this.launchHotjar()
+          if (this.$stores.auth.isLogged && this.$stores.auth.contextRole !== 'admin') {
+            this.identifyUser()
+          }
         }
-      h._hjSettings = { hjid: 2373674, hjsv: 6, site_id: 2373674 }
-      a = o.getElementsByTagName('head')[0]
-      r = o.createElement('script')
-      r.async = 1
-      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv
-      a.appendChild(r)
-    })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=')
-    // Hotjar script end
-
-    if (this.$stores.auth.isLogged && this.$stores.auth.contextRole !== 'admin') {
-      this.identifyUser()
-    }
+      })
+    })
   },
   computed: {
-    includeHotjar() {
+    shouldLaunchHotjar() {
       if (this.$stores.auth.isLogged && this.$stores.auth.contextRole === 'admin') {
         console.log('no hotjar for admin')
         return false
@@ -35,6 +27,23 @@ export default {
     },
   },
   methods: {
+    launchHotjar() {
+      // Hotjar script start
+      ;(function (h, o, t, j, a, r) {
+        h.hj =
+          h.hj ||
+          function () {
+            ;(h.hj.q = h.hj.q || []).push(arguments)
+          }
+        h._hjSettings = { hjid: 2373674, hjsv: 6, site_id: 2373674 }
+        a = o.getElementsByTagName('head')[0]
+        r = o.createElement('script')
+        r.async = 1
+        r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv
+        a.appendChild(r)
+      })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=')
+      // Hotjar script end
+    },
     identifyUser() {
       if (window.hj) {
         window.hj('identify', this.$stores.auth?.user?.id, {
