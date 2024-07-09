ca76bc62974e9e1eae6a052e4b5d42cccee06408
NVaissaud <n.vaissaud@gmail.com>
2024-07-09 17:21:52 +0200

    Exclude some errors from Flare

---
+++ components/section/dashboard/BoxOperations.vue
@@ -0,0 +1,47 @@
+<template>
+  <BaseBox variant="shadow" padding="sm">
+    <BaseHeading as="h2" :level="3" class="mb-8"> Nos opérations nationales </BaseHeading>
+
+    <div class="flex flex-col gap-1">
+      <div class="flex justify-center items-center gap-4">
+        <div class="text-[42px]">🌞</div>
+        <div class="flex-1" data-v-inspector="components/base/StackedListItem.vue:26:5">
+          <div class="text-gray-900 font-semibold">
+            Opérations canicules <DsfrBadge size="sm">En cours</DsfrBadge>
+          </div>
+          <div class="text-gray-600 text-sm">Du 1er juillet au 31 aout</div>
+        </div>
+      </div>
+      <div class="flex justify-center items-center gap-4">
+        <div class="text-[42px]">📚</div>
+        <div class="flex-1" data-v-inspector="components/base/StackedListItem.vue:26:5">
+          <div class="text-gray-900 font-semibold">
+            Septembre pour apprendre <DsfrBadge type="gray" size="sm">À venir</DsfrBadge>
+          </div>
+          <div class="text-gray-600 text-sm">Du 1er au 30 septembre</div>
+        </div>
+      </div>
+      <div class="flex justify-center items-center gap-4">
+        <div class="text-[42px]">🎄</div>
+        <div class="flex-1" data-v-inspector="components/base/StackedListItem.vue:26:5">
+          <div class="text-gray-900 font-semibold">
+            Décembre ensemble <DsfrBadge type="gray" size="sm">À venir</DsfrBadge>
+          </div>
+          <div class="text-gray-600 text-sm">Du 1er au 31 décembre</div>
+        </div>
+      </div>
+    </div>
+    <div class="mt-8"><DsfrButton type="tertiary">En savoir plus</DsfrButton></div>
+  </BaseBox>
+</template>
+
+<script>
+export default defineNuxtComponent({
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  methods: {},
+})
+</script>
--- plugins/flare.client.ts
+++ plugins/flare.client.ts
@@ -1,5 +1,14 @@
-import { flare } from '@flareapp/flare-client'
+import { flare } from '@flareapp/js'
 import { flareVue } from '@flareapp/flare-vue'
+
+const excludeContainsMessagesErrors = [
+  '403',
+  '401',
+  '422',
+  'Failed to fetch',
+  'dynamically imported module',
+  'Network Error',
+]
 
 export default defineNuxtPlugin(({ $config, vueApp }) => {
   if (!$config.public.flare.projectKey) {
@@ -8,6 +17,11 @@ export default defineNuxtPlugin(({ $config, vueApp }) => {
 
   flare.light($config.public.flare.projectKey)
   flare.beforeSubmit = (report: any) => {
+    // if message contains string in excludeContainsMessagesErrors, return false
+    if (excludeContainsMessagesErrors.some((message) => report.message.includes(message))) {
+      return false
+    }
+
     return {
       ...report,
       application_path: report.context.request?.url,
@@ -17,7 +31,7 @@ export default defineNuxtPlugin(({ $config, vueApp }) => {
   const { $stores } = useNuxtApp()
   if ($stores.auth.user?.profile) {
     flare.addContextGroup('user', $stores.auth.user?.profile)
-    flare.addContextGroup('currentRole', $stores.auth.currentRole)
+    flare.addContextGroup('currentRole', $stores.auth?.currentRole)
   }
 
   vueApp.use(flareVue)
