d3f2a1a1d4a7b268a11d10c62051c979a9f5111c
Ijahman <kevin.vaissaud@gmail.com>
2024-07-17 15:52:43 +0200

    Theme form info

--- components/base/FormInfo.vue
+++ components/base/FormInfo.vue
@@ -1,8 +1,6 @@
 <template>
-  <p class="text-xs text-[#0063CB] mt-2">
-    <DsfrInfoIcon
-      class="inline fill-current flex-none mr-2 object-contain w-4 h-4 translate-y-[-1px]"
-    />
+  <p class="text-xs text-[#0063CB] mt-3 flex">
+    <DsfrInfoIcon class="fill-current flex-none mr-2 object-contain w-4 h-4 translate-y-[1px]" />
     <slot />
   </p>
 </template>
