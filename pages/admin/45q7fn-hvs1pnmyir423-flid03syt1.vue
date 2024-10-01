7a73fc09f02b4ce7ca54bffd8fdfa079ca293e08
Ijahman <kevin.vaissaud@gmail.com>
2024-10-01 17:13:37 +0200

    Debug

---
+++ pages/admin/45q7fn-hvs1pnmyir423-flid03syt1.vue
@@ -0,0 +1,13 @@
+<template></template>
+
+<script>
+export default defineNuxtComponent({
+  async setup() {
+    definePageMeta({
+      middleware: ['admin'],
+    })
+
+    await useApiFetch('qsl3df-haz7uif5a-ozf44sqd9f-ai0eha243')
+  },
+})
+</script>
